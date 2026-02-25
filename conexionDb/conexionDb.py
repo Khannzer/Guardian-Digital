2947d7ea4c05056618449b1ba049605fa21b3f1a
Fernando Belizario <belizariof22@gmail.com>
2026-02-25 16:10:27 -0500

    mapa y chat dinamico implementados

--- conexionDb/conexionDb.py
+++ conexionDb/conexionDb.py
@@ -6,8 +6,8 @@ class ConexionDb:
     def conexionBaseDeDatos():
         try:
             conexion = mysql.connector.connect(
-                user='root',
-                password='mysql',
+                user='root', 
+                password='root',
                 host='localhost',
                 database='proyectoContraSuicidioV1',
                 port='3306'
@@ -17,3 +17,4 @@ class ConexionDb:
         except mysql.connector.Error as error:
             print("Error al conectar con la base de datos {}".format(error))
             return None
+        
